9c61b6648195bbf231ec5d0251fba85d0b3f9f7c
Hamidzuhdi <hamidwijanarko@gmail.com>
2024-11-14 09:27:54 +0700

    db

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template 
-from flaskext.mysql import MySQL
+from flask import Flask, render_template
+from flask_mysql_connector import MySQL
+
 app = Flask(__name__)
 
+# Konfigurasi database
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DATABASE'] = 'banksampah'
+app.config['MYSQL_HOST'] = 'localhost'
+
+mysql = MySQL(app)
+
+# Menampilkan homepage
 @app.route("/")
 def home():
     return render_template('index.html')
